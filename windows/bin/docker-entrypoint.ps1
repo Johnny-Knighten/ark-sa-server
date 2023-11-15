7ea20206ed52ed869f03b6334a94da86863f468f
Johnny Knighten <145606193+Johnny-Knighten@users.noreply.github.com>
2023-11-14 22:55:17 -0500

    feat: docker entrypoint now creates required sub directories

--- windows/bin/docker-entrypoint.ps1
+++ windows/bin/docker-entrypoint.ps1
@@ -4,4 +4,15 @@ if ($null -ne $env:DEBUG -and $env:DEBUG.ToLower() -ne "false" -and $env:DEBUG -
   Set-PSDebug -Trace 1
 }
 
-echo "Starting docker-entrypoint.ps1"
+$requiredSubDirs = @("server")
+
+foreach ($subDir in $requiredSubDirs) {
+    $dirPath = Join-Path $env:ARK_SERVER_DIR $subDir
+    if (-not (Test-Path $dirPath)) {
+        try {
+            New-Item -Path $dirPath -ItemType Directory -Force | Out-Null
+        } catch {
+            Write-Host "Failed to create directory: $dirPath"
+        }
+    }
+}
