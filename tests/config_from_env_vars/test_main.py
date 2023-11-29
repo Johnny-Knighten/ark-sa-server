8e86f109167c2add1a7ef42cbd7dd73a84d13b11
Johnny Knighten <johnny@knightenuniforms.com>
2023-11-29 16:44:05 -0500

    test: refactored config_from_env_vars unittests to use patch decorator

--- tests/config_from_env_vars/test_main.py
+++ tests/config_from_env_vars/test_main.py
@@ -16,124 +16,95 @@ class TestConfigFromEnvVars(unittest.TestCase):
     def tearDown(self):
         self.env_patcher.stop()
 
+    @patch.dict(
+        "os.environ",
+        {"CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25"},
+    )
     def test_process_env_vars_simple(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("ServerSettings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["ServerSettings"]["DifficultyOffset"], "0.25"
-            )
+        result = process_env_vars()
+        self.assertEqual(
+            result,
+            {"GameUserSettings": {"ServerSettings": {"DifficultyOffset": "0.25"}}},
+        )
 
+    @patch.dict(
+        "os.environ",
+        {
+            "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
+            "CONFIG_AppearanceSettings_ServerSettings_DifficultyOffset": "0.75",
+        },
+    )
     def test_process_env_vars_with_two_files(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
-                "CONFIG_AppearanceSettings_ServerSettings_DifficultyOffset": "0.75",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("ServerSettings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["ServerSettings"]["DifficultyOffset"],
-                "0.25",
-            )
-            self.assertIn("AppearanceSettings", result)
-            self.assertIn("ServerSettings", result["AppearanceSettings"])
-            self.assertEqual(
-                result["AppearanceSettings"]["ServerSettings"]["DifficultyOffset"],
-                "0.75",
-            )
+        result = process_env_vars()
+        expected = {
+            "GameUserSettings": {"ServerSettings": {"DifficultyOffset": "0.25"}},
+            "AppearanceSettings": {"ServerSettings": {"DifficultyOffset": "0.75"}},
+        }
+        self.assertEqual(result, expected)
 
+    @patch.dict(
+        "os.environ",
+        {
+            "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
+            "CONFIG_GameUserSettings_PlayerSettings_DifficultyOffset": "0.75",
+        },
+    )
     def test_process_env_vars_with_two_sections(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
-                "CONFIG_GameUserSettings_PlayerSettings_DifficultyOffset": "0.75",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("ServerSettings", result["GameUserSettings"])
-            self.assertIn("PlayerSettings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["ServerSettings"]["DifficultyOffset"],
-                "0.25",
-            )
-            self.assertEqual(
-                result["GameUserSettings"]["PlayerSettings"]["DifficultyOffset"],
-                "0.75",
-            )
+        result = process_env_vars()
+        expected = {
+            "GameUserSettings": {
+                "ServerSettings": {"DifficultyOffset": "0.25"},
+                "PlayerSettings": {"DifficultyOffset": "0.75"},
+            }
+        }
+        self.assertEqual(result, expected)
 
+    @patch.dict(
+        "os.environ",
+        {
+            "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
+            "CONFIG_GameUserSettings_ServerSettings_XPOffset": "0.75",
+        },
+    )
     def test_process_env_vars_with_two_vars(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_ServerSettings_DifficultyOffset": "0.25",
-                "CONFIG_GameUserSettings_ServerSettings_XPOffset": "0.75",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("ServerSettings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["ServerSettings"]["DifficultyOffset"],
-                "0.25",
-            )
-            self.assertEqual(
-                result["GameUserSettings"]["ServerSettings"]["XPOffset"],
-                "0.75",
-            )
+        result = process_env_vars()
+        expected = {
+            "GameUserSettings": {
+                "ServerSettings": {"DifficultyOffset": "0.25", "XPOffset": "0.75"}
+            }
+        }
+        self.assertEqual(result, expected)
 
+    @patch.dict(
+        "os.environ",
+        {"CONFIG_GameUserSettings_Server_DOT_Settings_DifficultyOffset": "0.25"},
+    )
     def test_process_env_vars_with_dot_in_section(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_Server_DOT_Settings_DifficultyOffset": "0.25",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("Server.Settings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["Server.Settings"]["DifficultyOffset"],
-                "0.25",
-            )
+        result = process_env_vars()
+        self.assertEqual(
+            result,
+            {"GameUserSettings": {"Server.Settings": {"DifficultyOffset": "0.25"}}},
+        )
 
+    @patch.dict(
+        "os.environ",
+        {"CONFIG_GameUserSettings_Server_SLASH_Settings_DifficultyOffset": "0.25"},
+    )
     def test_process_env_vars_with_slash_in_section(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_Server_SLASH_Settings_DifficultyOffset": "0.25",
-            },
-        ):
-            result = process_env_vars()
-            self.assertIn("GameUserSettings", result)
-            self.assertIn("Server/Settings", result["GameUserSettings"])
-            self.assertEqual(
-                result["GameUserSettings"]["Server/Settings"]["DifficultyOffset"],
-                "0.25",
-            )
+        result = process_env_vars()
+        self.assertEqual(
+            result,
+            {"GameUserSettings": {"Server/Settings": {"DifficultyOffset": "0.25"}}},
+        )
 
+    @patch.dict("os.environ", {"CONFIG_GameUserSettings_DifficultyOffset": "0.25"})
     def test_process_env_vars_with_missing_variable_template_section(self):
-        with patch.dict(
-            "os.environ",
-            {
-                "CONFIG_GameUserSettings_DifficultyOffset": "0.25",
-            },
-        ):
-            result = process_env_vars()
-            self.assertEqual(result, {})
+        result = process_env_vars()
+        self.assertEqual(result, {})
 
-    def test_update_ini_files_single_file(self):
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("config_from_env_vars.main.MaintainCaseConfigParser")
+    def test_update_ini_files_single_file(self, mock_config, mock_file):
         mock_config_data = {
             "GameUserSettings": {
                 "ServerSettings": {
@@ -141,22 +112,16 @@ class TestConfigFromEnvVars(unittest.TestCase):
                 }
             }
         }
+        update_ini_files(mock_config_data, "/fake/path")
+        mock_file.assert_called_once_with("/fake/path/GameUserSettings.ini", "w")
+        mock_config.return_value.set.assert_called_once_with(
+            "ServerSettings", "DifficultyOffset", "0.25"
+        )
+        mock_config.return_value.write.assert_called_once()
 
-        m = mock_open()
-        with patch("builtins.open", m), patch(
-            "config_from_env_vars.main.MaintainCaseConfigParser"
-        ) as mock_config:
-            update_ini_files(mock_config_data, "/fake/path")
-
-            m.assert_called_once_with("/fake/path/GameUserSettings.ini", "w")
-
-            mock_config.return_value.set.add_section("ServerSettings")
-            mock_config.return_value.set.assert_any_call(
-                "ServerSettings", "DifficultyOffset", "0.25"
-            )
-            mock_config.return_value.write.assert_called_once()
-
-    def test_update_ini_files_multiple_file(self):
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("config_from_env_vars.main.MaintainCaseConfigParser")
+    def test_update_ini_files_multiple_file(self, mock_config, mock_file):
         mock_config_data = {
             "GameUserSettings": {
                 "ServerSettings": {
@@ -169,38 +134,19 @@ class TestConfigFromEnvVars(unittest.TestCase):
                 }
             },
         }
+        update_ini_files(mock_config_data, "/fake/path")
+        mock_file.assert_any_call("/fake/path/GameUserSettings.ini", "w")
+        mock_file.assert_any_call("/fake/path/GameAppearance.ini", "w")
+        self.assertEqual(mock_config.return_value.write.call_count, 2)
 
-        m = mock_open()
-        with patch("builtins.open", m), patch(
-            "config_from_env_vars.main.MaintainCaseConfigParser"
-        ) as mock_config:
-            update_ini_files(mock_config_data, "/fake/path")
-
-            m.assert_any_call("/fake/path/GameUserSettings.ini", "w")
-            m.assert_any_call("/fake/path/GameAppearance.ini", "w")
-
-            mock_config.return_value.set.assert_any_call(
-                "ServerSettings", "DifficultyOffset", "0.25"
-            )
-            mock_config.return_value.set.assert_any_call(
-                "Logo", "Path", "/path/to/logo.png"
-            )
-
-            self.assertEqual(mock_config.return_value.write.call_count, 2)
-
-    def test_update_ini_files_no_data_to_write(self):
+    @patch("builtins.open", new_callable=mock_open)
+    @patch("config_from_env_vars.main.MaintainCaseConfigParser")
+    def test_update_ini_files_no_data_to_write(self, mock_config, mock_file):
         mock_config_data = {}
-
-        m = mock_open()
-        with patch("builtins.open", m), patch(
-            "config_from_env_vars.main.ConfigParser"
-        ) as mock_config:
-            update_ini_files(mock_config_data, "/fake/path")
-
-            m.assert_not_called()
-
-            mock_config.return_value.set.not_called()
-            mock_config.return_value.write.not_called()
+        update_ini_files(mock_config_data, "/fake/path")
+        mock_file.assert_not_called()
+        mock_config.return_value.set.assert_not_called()
+        mock_config.return_value.write.assert_not_called()
 
 
 if __name__ == "__main__":
